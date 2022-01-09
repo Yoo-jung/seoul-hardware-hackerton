995cba57c62501ce7b4d8678ed996af79b7c0565
gcuutakeout <97321411+gcuutakeout@users.noreply.github.com>
2022-01-09 10:36:46 +0900

    Add files via upload

---
+++ server/AWS/helloworld.js
@@ -0,0 +1,11 @@
+--2022-01-06 21:39:27--  http://raw.githubusercontent.com/yogeshraheja/Effective-DevOps-with-AWS/master/Chapther02/helloworld.js
+Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 185.199.110.133, 185.199.109.133, 185.199.108.133, ...
+Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|185.199.110.133|:80... failed: Resource temporarily unavailable.
+Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|185.199.109.133|:80... connected.
+HTTP request sent, awaiting response... 301 Moved Permanently
+Location: https://raw.githubusercontent.com/yogeshraheja/Effective-DevOps-with-AWS/master/Chapther02/helloworld.js [following]
+--2022-01-06 21:39:48--  https://raw.githubusercontent.com/yogeshraheja/Effective-DevOps-with-AWS/master/Chapther02/helloworld.js
+Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|185.199.109.133|:443... connected.
+HTTP request sent, awaiting response... 404 Not Found
+2022-01-06 21:39:49 ERROR 404: Not Found.
+
